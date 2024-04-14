5091bbdba2fb27867c17be52dea7dab6cfe97cd1
bhuwan <bkandpal.bk@gmail.com>
2024-04-14 21:36:45 +0530

    added code

--- force-app/main/default/lwc/searchAccount/searchAccount.js
+++ force-app/main/default/lwc/searchAccount/searchAccount.js
@@ -1,3 +1,34 @@
-import { LightningElement } from 'lwc';
+import  { LightningElement} from 'lwc';
+import getData from '@salesforce/apex/AccountDatatable.getData';
 
-export default class SearchAccount extends LightningElement {}
+const columns = [
+    {label:'Name', fieldName:'Name'},
+    {label:'Phone', fieldName:'Phone'}
+];
+
+
+export default class SearchAccount extends LightningElement {
+    // @track searchValue;
+    searchValue;
+    displayResult;
+    columns=columns;
+
+    searchHandler(event){
+        this.searchValue = event.target.value; 
+        this.ImperativeCall();
+    }
+        ImperativeCall(){
+            getData({searchInput:this.searchValue})
+            .then((result) => {
+                this.displayResult=result;
+                console.log("obj: " + JSON.stringify( this.displayResult));       
+            }
+        
+        )
+            .catch((error) => {
+                console.log('Error occured',error);
+            })      
+             
+            }  
+        }
+   
