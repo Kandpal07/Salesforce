6e8aea33a21f0b1a920a523d5f19a11c3d4212a4
bhuwan <bkandpal.bk@gmail.com>
2024-04-17 22:45:14 +0530

    concised code with handleInputChange method

--- force-app/main/default/lwc/searchAccount/searchAccount.js
+++ force-app/main/default/lwc/searchAccount/searchAccount.js
@@ -1,51 +1,41 @@
-
-import  { LightningElement} from 'lwc';
+import { LightningElement } from 'lwc';
 import getData from '@salesforce/apex/AccountDatatable.getData';
 
 const columns = [
-    {label:'Name', fieldName:'Name'},
-    {label:'Phone', fieldName:'Phone'}
+    { label: 'Name', fieldName: 'Name' },
+    { label: 'Phone', fieldName: 'Phone' }
 ];
-
 
 export default class SearchAccount extends LightningElement {
     searchValue;
     displayResult;
-    columns=columns;
+    columns = columns;
     accountName;
     accountPhone;
-      
-    handleNameInputChange(event){
-        const val = event.target.value.trim(); 
-        const words = val.split(' '); 
-        const finalWord = words[words.length - 1]; 
-        this.accountName=finalWord;
-        console.log('Final name :', this.accountName);
-    }
-    handlePhoneInputChange(event){
-        const val = event.target.value.trim(); 
-        const words = val.split(' '); 
-        const finalName = words[words.length - 1]; 
-        this.accountPhone=finalName;
-        console.log('Final phone :', this.accountPhone);
-    }
 
-
-    handleClick(event){
-        this.ImperativeCall();
-    }
-        ImperativeCall(){
-            console.log('ImperativeCall');
-            getData({inputName:this.accountName,inputPhone:this.accountPhone})
-            .then((result) => {
-                this.displayResult=result; 
-                console.log("Phone: "+ result.map(a=>a.Phone));
-            }
-        )
-            .catch((error) => {
-                console.log('Error occured',error);
-            })      
-             
-            }  
-        
+    handleInputChange(event) {
+        const fieldName = event.target.dataset.field;
+        const val = event.target.value.trim();
+        const words = val.split(' ');
+        const finalWord = words[words.length - 1];
+        if (fieldName === 'Name') {
+            this.accountName = finalWord;
+            console.log('Final name:', this.accountName);
+        } else if (fieldName === 'Phone') {
+            this.accountPhone = finalWord;
+            console.log('Final phone:', this.accountPhone);
         }
+    }
+
+    handleClick(event) {
+        console.log('ImperativeCall');
+        getData({ inputName: this.accountName, inputPhone: this.accountPhone })
+            .then(result => {
+                this.displayResult = result;
+                console.log("Phone: "+ result.map(a=>a.Phone));
+            })
+            .catch(error => {
+                console.log('Error occurred', error);
+            });
+    }
+}
